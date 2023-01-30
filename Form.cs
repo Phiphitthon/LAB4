da1d3fdd5529773eca739da23d03618e7b64ae3f
Phiphitthon <119285445+Phiphitthon@users.noreply.github.com>
2023-01-30 23:53:26 +0700

    Add files via upload

---
+++ Form.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LAB4
+{
+    public partial class Form1 : Form
+    {
+        Classroom classroom;
+        public form()
+        {
+            InitializeComponent();
+            classroom = new Classroom("LAB4");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string name = this.tbName.Text;
+            string birthyear = this.tbBirthYear.Text;
+            string grade = this.tbGrade.Text;
+
+            int bYear = Int32.Parse(birthyear);
+            double gpa = Double.Parse(grade);
+            // create object form Person class
+            Person person = new Person(name, bYear, gpa);
+            this.classroom.addPerson2Class(person);
+            //display person
+            this.tbTotal.Text = this.classroom.showAllPersoninClass();
+            //display total age of class
+            this.tbTotal1.Text = this.classroom.showsumageinClass().ToString();
+            // display avg Grade 
+            this.tbGrade.Text = this.classroom.avggradeinClass().ToString("#.##");
+
+            // display min max GPA
+            this.tbMax.Text = this.classroom.Maxgrade().ToString();
+            this.tbMin.Text = this.classroom.Mingrade().ToString();
+
+            // display name min max
+            this.tbNameMax.Text = this.classroom.shownamemaxGPA().ToString();
+            this.tbNameMin.Text = this.classroom.shownameminGPA().ToString();
+
+            // clear 
+            this.tbName.Text = "";
+            this.tbBirthYear.Text = "";
+            this.tbGrade.Text = "";
+
+
+
+
+
---
+++ Form1.Designer.cs
@@ -0,0 +1,296 @@
+﻿namespace LAB4
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tbName = new System.Windows.Forms.TextBox();
+            this.tbBirthYear = new System.Windows.Forms.TextBox();
+            this.tbGrade = new System.Windows.Forms.TextBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.tbNameMax = new System.Windows.Forms.Label();
+            this.tbNameMin = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.tbMax = new System.Windows.Forms.TextBox();
+            this.tbMin = new System.Windows.Forms.TextBox();
+            this.GradeAvg = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.tbTotal = new System.Windows.Forms.TextBox();
+            this.tbTotal1 = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(24, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(20, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "ชื่อ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(24, 73);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(31, 15);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "ปีเกิด";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(26, 100);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(29, 15);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "GPA";
+            // 
+            // tbName
+            // 
+            this.tbName.Location = new System.Drawing.Point(67, 36);
+            this.tbName.Name = "tbName";
+            this.tbName.Size = new System.Drawing.Size(100, 23);
+            this.tbName.TabIndex = 3;
+            // 
+            // tbBirthYear
+            // 
+            this.tbBirthYear.Location = new System.Drawing.Point(67, 65);
+            this.tbBirthYear.Name = "tbBirthYear";
+            this.tbBirthYear.Size = new System.Drawing.Size(100, 23);
+            this.tbBirthYear.TabIndex = 4;
+            // 
+            // tbGrade
+            // 
+            this.tbGrade.Location = new System.Drawing.Point(67, 97);
+            this.tbGrade.Name = "tbGrade";
+            this.tbGrade.Size = new System.Drawing.Size(100, 23);
+            this.tbGrade.TabIndex = 5;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(68, 134);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(99, 46);
+            this.btnAdd.TabIndex = 6;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.tbTotal1);
+            this.groupBox1.Controls.Add(this.tbTotal);
+            this.groupBox1.Controls.Add(this.label10);
+            this.groupBox1.Controls.Add(this.label9);
+            this.groupBox1.Controls.Add(this.textBox6);
+            this.groupBox1.Controls.Add(this.textBox5);
+            this.groupBox1.Controls.Add(this.GradeAvg);
+            this.groupBox1.Controls.Add(this.tbMin);
+            this.groupBox1.Controls.Add(this.tbMax);
+            this.groupBox1.Controls.Add(this.label8);
+            this.groupBox1.Controls.Add(this.label7);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.tbNameMin);
+            this.groupBox1.Controls.Add(this.tbNameMax);
+            this.groupBox1.Location = new System.Drawing.Point(186, 19);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(343, 217);
+            this.groupBox1.TabIndex = 7;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "ข้อมูลรายวิชา";
+            // 
+            // tbNameMax
+            // 
+            this.tbNameMax.AutoSize = true;
+            this.tbNameMax.Location = new System.Drawing.Point(16, 25);
+            this.tbNameMax.Name = "tbNameMax";
+            this.tbNameMax.Size = new System.Drawing.Size(55, 15);
+            this.tbNameMax.TabIndex = 8;
+            this.tbNameMax.Text = "GPA Max";
+            // 
+            // tbNameMin
+            // 
+            this.tbNameMin.AutoSize = true;
+            this.tbNameMin.Location = new System.Drawing.Point(16, 54);
+            this.tbNameMin.Name = "tbNameMin";
+            this.tbNameMin.Size = new System.Drawing.Size(53, 15);
+            this.tbNameMin.TabIndex = 9;
+            this.tbNameMin.Text = "GPA Min";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(16, 81);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(53, 15);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "GPA Avg";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(16, 115);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(41, 15);
+            this.label7.TabIndex = 8;
+            this.label7.Text = "อายุรวม";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(16, 145);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(36, 15);
+            this.label8.TabIndex = 11;
+            this.label8.Text = "รายชื่อ";
+            // 
+            // tbMax
+            // 
+            this.tbMax.Location = new System.Drawing.Point(77, 17);
+            this.tbMax.Name = "tbMax";
+            this.tbMax.Size = new System.Drawing.Size(100, 23);
+            this.tbMax.TabIndex = 8;
+            // 
+            // tbMin
+            // 
+            this.tbMin.Location = new System.Drawing.Point(77, 46);
+            this.tbMin.Name = "tbMin";
+            this.tbMin.Size = new System.Drawing.Size(100, 23);
+            this.tbMin.TabIndex = 12;
+            // 
+            // GradeAvg
+            // 
+            this.GradeAvg.Location = new System.Drawing.Point(77, 78);
+            this.GradeAvg.Name = "GradeAvg";
+            this.GradeAvg.Size = new System.Drawing.Size(100, 23);
+            this.GradeAvg.TabIndex = 13;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(77, 107);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(100, 23);
+            this.textBox5.TabIndex = 14;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(77, 137);
+            this.textBox6.Multiline = true;
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(100, 55);
+            this.textBox6.TabIndex = 15;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(195, 25);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(20, 15);
+            this.label9.TabIndex = 16;
+            this.label9.Text = "ชื่อ";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(195, 54);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(20, 15);
+            this.label10.TabIndex = 17;
+            this.label10.Text = "ชื่อ";
+            // 
+            // tbTotal
+            // 
+            this.tbTotal.Location = new System.Drawing.Point(221, 17);
+            this.tbTotal.Name = "tbTotal";
+            this.tbTotal.Size = new System.Drawing.Size(100, 23);
+            this.tbTotal.TabIndex = 18;
+            // 
+            // tbTotal1
+            // 
+            this.tbTotal1.Location = new System.Drawing.Point(221, 46);
+            this.tbTotal1.Name = "tbTotal1";
+            this.tbTotal1.Size = new System.Drawing.Size(100, 23);
+            this.tbTotal1.TabIndex = 19;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.tbGrade);
+            this.Controls.Add(this.tbBirthYear);
+            this.Controls.Add(this.tbName);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private TextBox tbName;
+        private TextBox tbBirthYear;
+        private TextBox tbGrade;
+        private Button btnAdd;
+        private GroupBox groupBox1;
+        private TextBox tbTotal1;
+        private TextBox tbTotal;
+        private Label label10;
+        private Label label9;
+        private TextBox textBox6;
+        private TextBox textBox5;
+        private TextBox GradeAvg;
+        private TextBox tbMin;
+        private TextBox tbMax;
+        private Label label8;
+        private Label label7;
+        private Label label6;
+        private Label tbNameMin;
+        private Label tbNameMax;
+    }
+}
